0b2e9f5ff7355733acdcaa7e39d2f499d2cc1208
Prof. Dr. Arif Wider <71143553+ProfWider@users.noreply.github.com>
2023-10-17 21:39:16 +0200

    typo

--- scripts/mesh_platform.py
+++ scripts/mesh_platform.py
@@ -1,7 +1,7 @@
 from catalog_connection import register_dp
 
 def create_dp(dp_spec):
-    catalog.register_dp(dp_spec)
+    register_dp(dp_spec)
 
 if __name__ == '__main__':
     create_dp("order_positions")
